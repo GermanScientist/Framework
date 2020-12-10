3c6af7e6738625f1be8e6502c6513e365a788486
GermanScientist <jack_hulspas@hotmail.com>
2020-12-10 17:08:21 +0100

    Removed sprites and added new sprite

--- demo/main.cpp
+++ demo/main.cpp
@@ -13,9 +13,7 @@ int main(void)
 {
     Renderer renderer(1280, 720);
 
-    Sprite* pencils = new Sprite("assets/pencils.tga");
-    Sprite* kingkong = new Sprite("assets/kingkong.tga");
-    Sprite* rgba = new Sprite("assets/rgba.tga");
+    Sprite* uvTemplate = new Sprite("assets/uvtemplate.tga");
     Cube* cube = new Cube("assets/uvtemplate.DDS");
 
     do {
@@ -33,11 +31,8 @@ int main(void)
 
         // Render all Sprites (Sprite*, xpos, ypos, xscale, yscale, rotation)
         static float rot_z = 0.0f;
-        renderer.renderSprite(pencils, 10, 12, 0.0f, 0.04f, 0.04f, 1.0f, 0.0f);
-        renderer.renderSprite(kingkong, -15, 5, 0.0f, 0.05f, 0.05f, 1.0f, 0.0f);
-        renderer.renderSprite(rgba, 0, -10, 0.0f, 0.04f, 0.04f, 1.0f, rot_z);
-        renderer.renderCube(cube, 0, 0, 0.0f, 3.0f, 3.0f, 3.0f, 0.0f);
-        rot_z += 3.141592f / 2 * deltaTime;
+        renderer.renderSprite(uvTemplate, 10, 0, 0.0f, 0.025f, 0.025f, 1.0f, 0.0f);
+        renderer.renderCube(cube, -10, 0, 0.0f, 3.0f, 3.0f, 3.0f, 0.0f);
 
         // Swap buffers
         glfwSwapBuffers(renderer.getWindow());
@@ -47,9 +42,7 @@ int main(void)
     while (glfwGetKey(renderer.getWindow(), GLFW_KEY_ESCAPE) != GLFW_PRESS &&
         glfwWindowShouldClose(renderer.getWindow()) == 0);
 
-    delete pencils;
-    delete kingkong;
-    delete rgba;
+    delete uvTemplate;
     delete cube;
 
     // Close OpenGL window and terminate GLFW
