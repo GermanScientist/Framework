27208b94ad65e6c98e9d93394cda1bb4bdc5e0de
GermanScientist <jack_hulspas@hotmail.com>
2021-01-13 15:01:14 +0100

    Added empty entity class

---
+++ myFramework/entity.cpp
@@ -0,0 +1,13 @@
+#include <myFramework/entity.h>
+
+//Constructor
+Entity::Entity()
+{
+
+}
+
+//Destructor
+Entity::~Entity()
+{
+
+}
---
+++ myFramework/entity.h
@@ -0,0 +1,22 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class Entity
+{
+	public:
+		Entity();
+		virtual ~Entity();
+
+	private:
+};
+
+#endif /* ENTITY_H */
