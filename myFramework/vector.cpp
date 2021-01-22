330db3995023c619a358614334167b311d8a3d5d
GermanScientist <jack_hulspas@hotmail.com>
2021-01-22 14:33:31 +0100

    Added empty vector class

---
+++ myFramework/vector.cpp
@@ -0,0 +1,13 @@
+#include <myFramework/vector.h>
+
+//Constructor
+Vector::Vector()
+{
+
+}
+
+//Destructor
+Vector::~Vector()
+{
+
+}
---
+++ myFramework/vector.h
@@ -0,0 +1,22 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class Vector
+{
+	public:
+		Vector();
+		virtual ~Vector();
+
+	private:
+};
+
+#endif /* VECTOR_H */
